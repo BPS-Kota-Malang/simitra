db637ffa7bd443a86d15065226e2a10b92f28f7f
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-10-25 01:48:32 +0700

    Membuat migrasi kelengkapan

---
+++ database/migrations/2022_10_19_023306_create_kelengkapan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelengkapan', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('tipe_id')->nullable();
+            $table->foreign('tipe_id')->references('id')->on('tipe_dokumen');
+            $table->string('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelengkapan');
+    }
+};
