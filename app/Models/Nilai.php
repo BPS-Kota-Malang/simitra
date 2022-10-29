c0186fb4a9065bf2e4d5c469326691e824d99651
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-10-30 01:32:54 +0700

    Membuat model nilai

---
+++ app/Models/Nilai.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Nilai extends Model
+{
+    use HasFactory;
+
+    protected $table = 'nilai';
+    protected $fillable = ['name'];
+}
