d14e5877e398f9ee47050dc590bc99d6ca8f12d1
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-11-14 13:34:36 +0700

    Migrasi tabel nilai

--- database/migrations/2022_10_29_175927_create_nilai_table.php
+++ database/migrations/2022_10_29_175927_create_nilai_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('nilai', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('id_users');
+            $table->foreign('id_users')->references('id')->on('users');
+            $table->unsignedBigInteger('id_kegiatan');
+            $table->foreign('id_kegiatan')->references('id')->on('kegiatan');
+            $table->unsignedBigInteger('id_kecamatan');
+            $table->foreign('id_kecamatan')->references('id')->on('kecamatan');
+            $table->unsignedBigInteger('id_sub_kecamatan');
+            $table->foreign('id_sub_kecamatan')->references('id')->on('sub_kecamatan');
             $table->double('nilai_ketepatan')->nullable();
             $table->double('nilai_kualitas')->nullable();
             $table->double('nilai_sikap')->nullable();
-            $table->string('status')->nullable();
             $table->timestamps();
         });
     }
