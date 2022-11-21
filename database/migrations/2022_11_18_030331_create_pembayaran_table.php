dc183019c75a51653ca0953dd8eb8830ced3139a
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-11-21 10:26:32 +0700

    Modifikasi migrasi pembayaran

---
+++ database/migrations/2022_11_18_030331_create_pembayaran_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_users')->nullable();
+            $table->foreign('id_users')->references('id')->on('users');
+            $table->unsignedBigInteger('id_kegiatan');
+            $table->foreign('id_kegiatan')->references('id')->on('kegiatan');
+            $table->unsignedBigInteger('id_kecamatan');
+            $table->foreign('id_kecamatan')->references('id')->on('kecamatan');
+            $table->unsignedBigInteger('id_sub_kecamatan');
+            $table->foreign('id_sub_kecamatan')->references('id')->on('sub_kecamatan');
+            $table->string('gaji')->nullable();
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+};
