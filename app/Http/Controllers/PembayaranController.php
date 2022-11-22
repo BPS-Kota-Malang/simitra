06ae0e2bc5996ef3ae7ceae8750acad81f4545c2
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-11-22 08:02:17 +0700

    Modifikasi Pembayaran controller

--- app/Http/Controllers/PembayaranController.php
+++ app/Http/Controllers/PembayaranController.php
@@ -20,6 +20,60 @@ class PembayaranController extends Controller
         return view('pembayaran.index', compact('pembayaran'), ['user' => $user, 'type_menu' => '']);
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $user = Auth::user();
+        $pembayaran = Pembayaran::find($id);
+        return view('pembayaran.show', compact('pembayaran'),['user' => $user, 'type_menu' => '']);
+    }
+
+    public function edit($id)
+    {
+        $user = Auth::user();
+        $pembayaran = Pembayaran::find($id);
+        return view('pembayaran.edit', compact('pembayaran'),['user' => $user, 'type_menu' => 'dashboard']);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $pembayaran= Pembayaran::find($id);
+        // $pembayaran->id_users=$user->id;
+        $pembayaran->id_kegiatan=$request->kegiatan;
+        $pembayaran->id_kecamatan=$request->kecamatan;
+        $pembayaran->gaji=$request->gaji;
+        $pembayaran->save();
+
+        $pembayaran->save();
+        return redirect()->route('pembayaran.index')->with('success', 'Data Pembayaarn berhasil di Simpan');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $pembayaran  = Pembayaran::find($id);
+        $pembayaran ->delete();
+        return redirect()->route('pembayaran.index')
+                        ->with('success','Data Pembayaran Berhasil Dihapus');
+    }
+
     public function changeStatus($id){
         $getStatus = Pembayaran::select('status')->where('id',$id)->first()  ;
         if($getStatus->status==1){
