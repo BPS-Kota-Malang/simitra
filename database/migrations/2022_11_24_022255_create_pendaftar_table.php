7cae531da53b8ba7a64ec747e15df6fc12842292
MutiaraRahmanda <mutiararizkia18@gmail.com>
2022-11-24 11:59:53 +0700

    Membuat migrasi pendaftar

---
+++ database/migrations/2022_11_24_022255_create_pendaftar_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_users')->nullable();
+            $table->foreign('id_users')->references('id')->on('users');
+            $table->unsignedBigInteger('id_kegiatan');
+            $table->foreign('id_kegiatan')->references('id')->on('kegiatan');
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftar');
+    }
+};
