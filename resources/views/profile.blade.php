15880bfefb1b84a139ee281b17ea8440f2acf44b
darman1725 <darmansaragih1701@gmail.com>
2022-12-16 22:56:27 +0700

    Menangkap data jabatan, agama, jenis kelamin, kecamatan dan kelurahan, dan menampilkan sensus

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -65,13 +65,15 @@
                             <li class="list-group-item">
                                 <div class="row">
                                     <div class="col-md-4">Kecamatan</div>
-                                    <div class="col-md-6 font-weight-bold">{{$user->guestKecamatan->kecamatan_tipe}}</div>
+                                    <div class="col-md-6 font-weight-bold">{{$user->guestKecamatan->kecamatan_tipe}}
+                                    </div>
                                 </div>
                             </li>
                             <li class="list-group-item">
                                 <div class="row">
                                     <div class="col-md-4">Kelurahan</div>
-                                    <div class="col-md-6 font-weight-bold">{{$user->guestKelurahan->sub_kecamatan}}</div>
+                                    <div class="col-md-6 font-weight-bold">{{$user->guestKelurahan->sub_kecamatan}}
+                                    </div>
                                 </div>
                             </li>
                             <li class="list-group-item">
@@ -84,6 +86,12 @@
                                 <div class="row">
                                     <div class="col-md-4">RW</div>
                                     <div class="col-md-6 font-weight-bold">{{$user->rw}}</div>
+                                </div>
+                            </li>
+                            <li class="list-group-item">
+                                <div class="row">
+                                    <div class="col-md-4">Pengalaman Sensus</div>
+                                    <div class="col-md-6 font-weight-bold">{{$user->pgl_sensus}}</div>
                                 </div>
                             </li>
                         </div>
@@ -133,29 +141,29 @@
                                 <div class="row">
                                     <div class="form-group col-md-10 col-1">
                                         <label>Alamat</label>
-                                        <textarea class="form-control" rows="3" value="{{ $user->alamat }}"
-                                            name="alamat" required=""
-                                            placeholder="Silahkan masukkan alamat anda .."></textarea>
+                                        <input type="text" class="form-control" value="{{ $user->alamat }}"
+                                            name="alamat" required="" placeholder="Silahkan masukkan alamat anda ..">
                                     </div>
                                 </div>
                                 <div class="row">
                                     <div class="form-group col-md-10 col-12">
                                         <label>Jabatan</label>
-                                        <select class="form-control py-0" name="jabatan" required=""
-                                            value="{{ $user->role }}">
-                                            <option selected disabled>Pilih Jabatan</option>
+                                        <select class="form-control py-0" name="jabatan" required="">
+                                            <option disabled value>Pilih Jabatan</option>
+                                            <option value="{{$user->role}}">{{$user->guestRole->name}}</option>
                                             @foreach ($role as $r)
                                             <option value="{{ $r->id }}">{{$r->name}}</option>
                                             @endforeach
                                         </select>
                                     </div>
                                 </div>
+
                                 <div class="row">
                                     <div class="form-group col-md-10 col-12">
                                         <label>Agama</label>
-                                        <select class="form-control py-0" name="agama" required=""
-                                            value="{{ $user->agama }}">
-                                            <option selected disabled>Pilih Agama</option>
+                                        <select class="form-control py-0" name="agama" required="">
+                                            <option disabled value>Pilih Agama</option>
+                                            <option>{{ $user->agama }}</option>
                                             <option value="Islam">Islam</option>
                                             <option value="Kristen">Kristen</option>
                                             <option value="Katolik">Katolik</option>
@@ -167,9 +175,9 @@
                                 <div class="row">
                                     <div class="form-group col-md-10 col-12">
                                         <label>Jenis Kelamin</label>
-                                        <select class="form-control py-0" name="jenis_kelamin" required=""
-                                            value="{{ $user->jenis_kelamin }}">
-                                            <option selected disabled>Pilih Jenis Kelamin</option>
+                                        <select class="form-control py-0" name="jenis_kelamin" required="">
+                                            <option disabled value>Pilih Jenis Kelamin</option>
+                                            <option>{{ $user->jenis_kelamin }}</option>
                                             <option value="Pria">Pria</option>
                                             <option value="Wanita">Wanita</option>
                                         </select>
@@ -178,9 +186,10 @@
                                 <div class="row">
                                     <div class="form-group col-md-10 col-12">
                                         <label>Kecamatan</label>
-                                        <select class="form-control py-0" name="kecamatan" required=""
-                                            value="{{ $user->kecamatan }}">
-                                            <option selected disabled>Pilih Kecamatan</option>
+                                        <select class="form-control py-0" name="kecamatan" required="">
+                                            <option disabled value>Pilih Kecamatan</option>
+                                            <option value="{{$user->kecamatan}}">
+                                                {{$user->guestKecamatan->kecamatan_tipe}}</option>
                                             @foreach ($kecamatan as $item)
                                             <option value="{{ $item->id }}">{{$item->kecamatan_tipe}}</option>
                                             @endforeach
@@ -190,9 +199,10 @@
                                 <div class="row">
                                     <div class="form-group col-md-10 col-12">
                                         <label>Kelurahan</label>
-                                        <select class="form-control py-0" name="kelurahan" required=""
-                                            value="{{ $user->kelurahan }}">
-                                            <option selected disabled>Pilih Kelurahan</option>
+                                        <select class="form-control py-0" name="kelurahan" required="">
+                                            <option disabled value>Pilih Kelurahan</option>
+                                            <option value="{{$user->kelurahan}}">
+                                                {{$user->guestKelurahan->sub_kecamatan}}</option>
                                             @foreach ($sub_kecamatan as $item)
                                             <option value="{{ $item->id }}">{{$item->sub_kecamatan}}</option>
                                             @endforeach
@@ -217,10 +227,10 @@
                                     <div class="form-group col-md-10 col-12">
                                         <label>Pengalaman Sensus/Survei BPS</label>
                                         <br>
-                                        <input type="checkbox" name="pengalaman[]" value="SP"> &nbsp; Sensus Penduduk
+                                        <input type="checkbox" name="pengalaman[]" value="Sensus Penduduk"> &nbsp; Sensus Penduduk
                                         2020 (SP2020)
                                         <br>
-                                        <input type="checkbox" name="pengalaman[]" value="SE"> &nbsp; Sensus Ekonomi
+                                        <input type="checkbox" name="pengalaman[]" value="Sensus Ekonomi"> &nbsp; Sensus Ekonomi
                                         2016 (SE2016)
                                         <br>
                                         <input type="checkbox" name="pengalaman[]" value="Susenas"> &nbsp; Survei Sosial
@@ -234,7 +244,6 @@
                                         <br>
                                         <input type="checkbox" name="pengalaman[]" value="Belum"> &nbsp; Belum Pernah
                                         mengikuti Survei / Sensus BPS
-
                                     </div>
                                 </div>
                                 <div class="row">
